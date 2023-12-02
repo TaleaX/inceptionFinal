eca9907d5b82854b29bac6a742b0d9f11b936566
TaleaX <sophie.dehne01@gmail.com>
2023-12-02 13:55:34 +0100

    mariadb user change

--- Makefile
+++ Makefile
@@ -14,6 +14,10 @@ build: createDir
 forceBuild: createDir
 	@sudo docker compose  -f ./srcs/docker-compose.yml up --build -d
 
+
+mariadb: createDir
+	@sudo docker compose -f ./srcs/docker-compose.yml up mariadb --build -d
+
 nginxShell:
 	@sudo docker exec -it nginx bash
 
--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,13 +1,12 @@
 service mariadb start
 
-#mariadb -e "CREATE DATABASE ${DB_NAME};"
-#mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-#mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
-#mariadb -e "FLUSH PRIVILEGES;"
-#mariadb-admin -u root password "${DB_ROOT_PASSWORD}"
-
-mariadb -e "CREATE DATABASE wordpress;"
-mariadb -e "CREATE USER 'tdehne'@'%' IDENTIFIED BY '1234';"
-mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'tdehne'@'%';"
+mariadb -e "CREATE DATABASE ${DB_NAME};"
+mariadb -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
-mariadb-admin -u root password "1234"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+service mariadb stop
+
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "user=mysql" >> /etc/mysql/my.cnf
